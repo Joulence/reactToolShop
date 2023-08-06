2eb33dcda903273047b97d5d5a3b43fa83e779bf
Joulence <4erviken@gmail.com>
2023-08-06 15:31:12 +0300

    Tools component added to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
+import Tools from "./components/Tools/Tools";
 
 function App() {
   return (
     <>
       <Header />
+      <Tools />
     </>
   );
 }
