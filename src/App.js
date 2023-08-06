402684a1f1807033951607e9ee6ae82bb5461ec9
Joulence <4erviken@gmail.com>
2023-08-06 16:30:37 +0300

    Added close cart functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import Header from "./components/Layout/Header";
 import Tools from "./components/Tools/Tools";
 import Cart from "./components/Cart/Cart";
+import { useState } from "react";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(true);
+
+  const showCartHandler = () => {
+    setCartIsShown(true);
+  };
+
+  const hideCartHandler = () => {
+    setCartIsShown(false);
+  };
+
   return (
     <>
-      <Cart />
-      <Header />
+      {cartIsShown && <Cart onClose={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Tools />
       </main>
