af7696343d4664ded69276a11fe3d35a7226b3ce
Joulence <4erviken@gmail.com>
2023-08-06 16:13:35 +0300

    Added Cart component into

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Layout/Header";
 import Tools from "./components/Tools/Tools";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Tools />
