809e2054b721c9a2b9913751172eeee9d2a906c7
Joulence <4erviken@gmail.com>
2023-08-06 15:31:00 +0300

    Updated Tools component

---
+++ src/components/Tools/Tools.js
@@ -0,0 +1,13 @@
+import ToolsSummary from "./ToolsSummary";
+import AvailableTools from "./AvailableTools";
+
+const Tools = () => {
+  return (
+    <>
+      <ToolsSummary />
+      <AvailableTools />
+    </>
+  );
+};
+
+export default Tools;
