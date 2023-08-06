5420c32e89a65af85f2a9ba301d3d5244534b833
Joulence <4erviken@gmail.com>
2023-08-06 15:42:33 +0300

    Created and added ToolItem component

---
+++ src/components/Tools/ToolItem/ToolItem.js
@@ -0,0 +1,20 @@
+import styles from "./ToolItem.module.scss";
+
+const ToolItem = (props) => {
+  const price = `$${props.price.toFixed(2)}`;
+
+  return (
+    <li className={styles.tool}>
+      <div>
+        <h3>{props.name}</h3>
+        <div className={styles.description}>{props.description}</div>
+        <div className={styles.price}>{price}</div>
+      </div>
+      <div>
+
+      </div>
+    </li>
+  );
+};
+
+export default ToolItem;
