c9494f7688d88ef07a61d2763a7ccf03851f37cb
Joulence <4erviken@gmail.com>
2023-08-06 15:47:17 +0300

    Created ToolItemForm component

---
+++ src/components/Tools/ToolItem/ToolItemForm.js
@@ -0,0 +1,12 @@
+import styles from "./ToolItemForm.module.scss";
+
+const ToolItemForm = (props) => {
+  return (
+    <form>
+      <input type="text" />
+      <button>+ Add</button>
+    </form>
+  );
+};
+
+export default ToolItemForm;
