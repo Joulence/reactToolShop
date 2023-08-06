e39d9f6d86f320736e661af540d73edbc99f06c9
Joulence <4erviken@gmail.com>
2023-08-06 15:25:39 +0300

    Created ToolsSummary component

---
+++ src/components/Tools/ToolsSummary.js
@@ -0,0 +1,19 @@
+import styles from "./ToolsSummary.module.scss";
+
+const ToolsSummary = () => {
+  return (
+    <section className={styles.summary}>
+      <h2>Awesome Tools, Delivered To You!</h2>
+      <p>
+        Unlock the power of innovation and efficiency with our exceptional
+        collection of tools designed to elevate your projects to new heights.
+      </p>
+      <p>
+        Whether you're a seasoned professional or just starting on your journey,
+        our curated selection will meet all your needs.
+      </p>
+    </section>
+  );
+};
+
+export default ToolsSummary;
